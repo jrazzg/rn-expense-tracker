56b544925142b5bf22d81509210c31ee3dd478f3
jrazzg <129245648+jrazzg@users.noreply.github.com>
2025-09-15 18:06:33 +0800

    Add category option

--- app/AddScreen.tsx
+++ app/AddScreen.tsx
@@ -7,20 +7,38 @@ const AddScreen = () => {
     const expenseContext = useContext(ExpenseContext);
     if (!expenseContext) throw new Error('Context not available')
     const { totalExpense, addExpense } = expenseContext;
-
     const [expense, setExpense] = useState<number>(0);
+
+    type ExpenseCategory = 'Alpha' | 'Beta' | 'Sigma';
+    const myCategory: ExpenseCategory[] = ['Alpha', 'Beta', 'Sigma'];
+    const [selectedCat, setSelectedCat] = useState<ExpenseCategory>('Sigma');
+
     return (
         <View>
             <Text>Total Expense: {totalExpense}</Text>
+            <Text style={styles.title}>Amount</Text>
             <TextInput
                 style={styles.textInput}
                 value={expense.toString()}
                 onChangeText={val => setExpense(Number(val))}
                 keyboardType='numeric'
                 inputMode='numeric' />
-            <TouchableOpacity onPress={() => addExpense(expense)}>
-                <Text>+</Text>
-            </TouchableOpacity>
+
+            <Text style={styles.title}>Category</Text>
+            {
+                myCategory.map((item) =>
+                    <TouchableOpacity
+                        onPress={() => setSelectedCat(item)}
+                        // key={item}
+                        style={{
+                            backgroundColor: selectedCat == item ? 'rgba(80, 156, 231, 1)' : 'white'
+                        }}
+                    >
+                        <Text>{item}</Text>
+                    </TouchableOpacity>
+                )
+            }
+            <TouchableOpacity onPress={() => addExpense(expense)}><Text>Add</Text></TouchableOpacity>
         </View>
     );
 };
@@ -30,5 +48,9 @@ export default AddScreen;
 const styles = StyleSheet.create({
     textInput: {
         backgroundColor: '#adadadff'
+    },
+    title: {
+        fontSize: 20,
+        marginTop: 20,
     }
 })
