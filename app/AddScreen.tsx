75d6d5f8212f4e1555c1f275b4cdaa6a53444163
jrazzg <129245648+jrazzg@users.noreply.github.com>
2025-10-01 02:15:39 +0800

    Format AddScreen UI

--- app/AddScreen.tsx
+++ app/AddScreen.tsx
@@ -9,19 +9,18 @@ const AddScreen = () => {
     const { totalExpense, addExpense, expenseList } = expenseContext;
     const [expense, setExpense] = useState<number>(0);
 
-    type ExpenseCategory = 'Alpha' | 'Beta' | 'Sigma';
-    const myCategory: ExpenseCategory[] = ['Alpha', 'Beta', 'Sigma'];
-    const [selectedCategory, setSelectedCategory] = useState<ExpenseCategory>('Sigma');
+    type ExpenseCategory = 'Food' | 'Transport' | 'Bills';
+    const myCategory: ExpenseCategory[] = ['Food', 'Transport', 'Bills'];
+    const [selectedCategory, setSelectedCategory] = useState<ExpenseCategory>('Food');
 
     const updateTryArray = (exp: number, cat: string) => {
         addExpense(exp, cat)
-        setSelectedCategory('Sigma')
+        setSelectedCategory('Food')
         setExpense(0)
     };
 
     return (
-        <View>
-            <Text>Total Expense: {totalExpense}</Text>
+        <View style={styles.container}>
             <Text style={styles.title}>Amount</Text>
             <TextInput
                 style={styles.textInput}
@@ -31,27 +30,24 @@ const AddScreen = () => {
                 inputMode='numeric' />
 
             <Text style={styles.title}>Category</Text>
+            <View style={styles.categoryContainer}>
             {
                 myCategory.map((item, index) =>
                     <TouchableOpacity
                         key={index}
                         onPress={() => setSelectedCategory(item)}
                         // key={item}
-                        style={{
-                            backgroundColor: selectedCategory == item ? 'rgba(80, 156, 231, 1)' : 'white'
-                        }}
+                        style={[
+                            styles.category,
+                            { backgroundColor: selectedCategory === item ? '#509ce7ff' : '#e6e6e6ff' }
+                        ]}
                     >
-                        <Text>{item}</Text>
+                        <Text style={styles.categoryText}>{item}</Text>
                     </TouchableOpacity>
                 )
             }
-            {/* <TouchableOpacity onPress={() => addExpense(expense)}><Text>Add</Text></TouchableOpacity> */}
-            <TouchableOpacity onPress={() => updateTryArray(expense, selectedCategory)}><Text>Add</Text></TouchableOpacity>            
-            {
-                expenseList.map((item, index) => (
-                    <Text key={index} style={[styles.title, { color: 'red' }]}>{item.expense} | {item.category}</Text>
-                ))
-            }
+            </View>
+            <TouchableOpacity onPress={() => updateTryArray(expense, selectedCategory)}><Text style={styles.button}>Add</Text></TouchableOpacity>            
         </View>
     );
 };
@@ -59,11 +55,43 @@ const AddScreen = () => {
 export default AddScreen;
 
 const styles = StyleSheet.create({
+    container: {
+        padding: 20,
+        backgroundColor: '#ffffffff',
+        flex: 1,
+    },
     textInput: {
-        backgroundColor: '#adadadff'
+        backgroundColor: '#e6e6e6ff',
+        borderRadius: 5,
+        paddingLeft: 10,
     },
     title: {
         fontSize: 20,
         marginTop: 20,
-    }
+        marginBottom: 5,
+    },
+    categoryContainer: {
+        flexDirection: 'row',
+    },
+    category: {
+        padding: 10,
+        marginRight: 10,
+        borderRadius: 5,
+    },
+    categoryText: {
+        textTransform: 'uppercase',
+    },
+    button: {
+        marginTop: 20,
+        padding: 10,
+        paddingHorizontal: 30,
+        backgroundColor: '#509ce7ff',
+        alignItems: 'center',
+        borderRadius: 5,
+        alignSelf: 'flex-end',
+        textTransform: 'uppercase',
+        textAlign: 'center',
+        fontWeight: '800',
+        color: 'white',
+    },
 })
